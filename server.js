f998676165f0805d4aa7cd5140cbe284f72e7763
Katie Chu <katiechux@gmail.com>
2023-02-18 11:22:58 -0800

    added partner and promotions routers

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const res = require('express/lib/response');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = '3000';
@@ -12,6 +14,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
